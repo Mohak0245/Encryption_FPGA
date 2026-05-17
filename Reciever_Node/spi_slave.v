25e1eecdfeb9b170d6dc4b138e88aaec7016e53a
Mohak0245 <mohakagarwal66.24@kgpian.iitkgp.ac.in>
2026-05-17 19:17:20 +0530

    Add files via upload

---
+++ Reciever_Node/spi_slave.v
@@ -0,0 +1,36 @@
+module spi_slave (
+    input wire SCLK,
+    input wire rst,
+    input wire MOSI,
+    input wire CS_bar,
+    output reg [7:0] received_data,
+    output reg data_ready
+);
+
+    reg [2:0] count; 
+
+    always @(posedge SCLK or posedge rst or posedge CS_bar) begin
+        if(rst || CS_bar) begin
+            received_data <= 0;
+            data_ready <= 0;
+            count <= 0;
+        end
+        else begin
+            if(CS_bar) begin
+                count <= 0;
+                data_ready <= 0;
+            end
+            else begin
+                data_ready <= 0;
+                received_data[7 - count] <= MOSI;
+                if(count == 3'b111) begin
+                    count <= 0;
+                    data_ready <= 1'b1;
+                end else begin
+                    count <= count + 1'b1;
+                end
+            end
+        end
+    end
+
+endmodule
